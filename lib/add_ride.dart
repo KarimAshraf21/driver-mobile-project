db6c35ec1443dcf78332010eb572260dde32d7ea
Karim Ashraf <karimashraf0055@gmail.com>
2023-12-14 16:46:32 +0200

    start or destination must be campus

--- lib/add_ride.dart
+++ lib/add_ride.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: avoid_print, unused_import
+// ignore_for_file: avoid_print, unused_import, use_build_context_synchronously
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import the intl package
 import '/home_page.dart';
 
 class AddPage extends StatefulWidget {
@@ -16,9 +17,14 @@ class _AddPageState extends State<AddPage> {
   List<String> locations = ['Campus', 'Nasr City 1', 'Tagamoa', 'Maadi'];
   String selectedStartLocation = "Campus";
   String selectedDestination = 'Nasr City 1';
+  int selectedSeats = 1;
+  String selectedTime = '5:30pm';
+  double selectedPrice = 0.0;
+  DateTime selectedDate = DateTime.now();
+
   final GlobalKey<FormState> _formKey = GlobalKey();
-  final TextEditingController _availableSeats = TextEditingController();
-  final TextEditingController _controllerTime = TextEditingController();
+  final TextEditingController _controllerPrice = TextEditingController();
+  final TextEditingController _controllerDate = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -42,40 +48,112 @@ class _AddPageState extends State<AddPage> {
                     key: _formKey,
                     child: Column(
                       children: [
-                        TextFormField(
-                          controller: _controllerTime,
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.access_time),
-                            filled: true,
-                            fillColor: Colors.white70,
-                            hintText: "add time",
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Can't be Empty";
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        TextFormField(
-                          controller: _availableSeats,
+                        DropdownButtonFormField<int>(
+                          value: selectedSeats,
                           decoration: const InputDecoration(
                             icon: Icon(Icons.event_seat_sharp),
                             filled: true,
                             fillColor: Colors.white70,
-                            hintText: "add available seats",
+                            hintText: "Choose available seats",
                           ),
+                          items: [1, 2, 3].map((int seats) {
+                            return DropdownMenuItem<int>(
+                              value: seats,
+                              child: Text(seats.toString()),
+                            );
+                          }).toList(),
+                          onChanged: (int? value) {
+                            setState(() {
+                              selectedSeats = value!;
+                            });
+                          },
                           validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Can't be Empty";
+                            if (value == null) {
+                              return "Please choose the number of available seats";
                             } else {
                               return null;
                             }
                           },
                         ),
-                        // Remove TextFormField widgets for start and end points
-                        // Add other text form fields as needed
+                        const SizedBox(height: 16),
+                        DropdownButtonFormField<String>(
+                          value: selectedTime,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.access_time),
+                            filled: true,
+                            fillColor: Colors.white70,
+                            hintText: "Choose time",
+                          ),
+                          items: ['5:30pm', '7:00am'].map((String time) {
+                            return DropdownMenuItem<String>(
+                              value: time,
+                              child: Text(time),
+                            );
+                          }).toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              selectedTime = value!;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null) {
+                              return "Please choose the time";
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          controller: _controllerPrice,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.attach_money),
+                            filled: true,
+                            fillColor: Colors.white70,
+                            hintText: "Enter price",
+                          ),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter the price";
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          controller: _controllerDate,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.date_range),
+                            filled: true,
+                            fillColor: Colors.white70,
+                            hintText: "Enter date",
+                          ),
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2101),
+                            );
+                            if (pickedDate != null &&
+                                pickedDate != selectedDate) {
+                              setState(() {
+                                selectedDate = pickedDate;
+                                _controllerDate.text =
+                                    DateFormat('dd-MM-yyyy').format(pickedDate);
+                              });
+                            }
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter the date";
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
                       ],
                     ),
                   ),
@@ -107,7 +185,6 @@ class _AddPageState extends State<AddPage> {
                     },
                   ),
                 ),
-                //////////////////////////////////////////////////////////////
                 ElevatedButton(
                   style: ButtonStyle(
                     backgroundColor:
@@ -115,6 +192,27 @@ class _AddPageState extends State<AddPage> {
                   ),
                   onPressed: () async {
                     if (_formKey.currentState!.validate()) {
+                      // Validate if either the start or destination is "Campus"
+                      if (selectedStartLocation != 'Campus' &&
+                          selectedDestination != 'Campus') {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                'Either start or destination should be Campus.'),
+                          ),
+                        );
+                        return;
+                      }
+                      if (selectedStartLocation == selectedDestination) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                'Start and destination should be different.'),
+                          ),
+                        );
+                        return;
+                      }
+
                       try {
                         // Access the Firestore instance
                         FirebaseFirestore firestore =
@@ -128,10 +226,12 @@ class _AddPageState extends State<AddPage> {
                           DocumentReference rideReference =
                               await firestore.collection('rides').add({
                             'driverId': user.uid,
-                            'time': _controllerTime.text,
+                            'time': selectedTime,
                             'start': selectedStartLocation,
                             'end': selectedDestination,
-                            'availableSeats': int.parse(_availableSeats.text),
+                            'availableSeats': selectedSeats,
+                            'price': double.parse(_controllerPrice.text),
+                            'date': selectedDate,
                           });
 
                           // Add the 'bookings' subcollection inside the ride document
@@ -139,9 +239,15 @@ class _AddPageState extends State<AddPage> {
                             // Add booking details as needed
                           });
 
-                          _controllerTime.clear();
-                          // Clearing other text form fields as needed
-                          _availableSeats.clear();
+                          // Clearing form fields
+                          setState(() {
+                            selectedSeats = 1;
+                            selectedTime = '5:30pm';
+                            selectedPrice = 0.0;
+                            selectedDate = DateTime.now();
+                            _controllerPrice.clear();
+                            _controllerDate.clear();
+                          });
 
                           // Navigate back to the previous screen
                           Navigator.pop(context);
